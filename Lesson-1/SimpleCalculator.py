5e09ac2806255cd44d27f818d260f924028b6bee
AllCoolNicknamesWereTaken <oliwia_bar@wp.pl>
2018-07-16 19:52:45 +0200

    robie sb

---
+++ Lesson-1/SimpleCalculator.py
@@ -0,0 +1,14 @@
+print 7 + 8, 12+ 4, 5 * 7 + 3, 5 * (7 + 3), 10 / 5, 7 / 2, 7 // 2, 7 % 2, 7 ** 5;
+ # _ + 5;
+# _ ** 2;
+#strings
+print('"Isn\'t," they said.');
+s = 'First line.\nSecond line.'  # \n means newline
+print s;
+print('C:\some\name');
+print(r'C:\some\name')
+print("dluuuuuuuu"
+    "uuuuuuuugi napis")
+print s[7];
+print s[0:7];
+print s[-7:];
